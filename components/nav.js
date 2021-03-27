bc5d525fc731db46d72d128133b3cc7711c91a09
sameghosts <seanfeiner.brown@gmail.com>
2021-03-26 21:42:57 -0400

    some stubbing index comp and main component containers, tailwind init

---
+++ components/nav.js
@@ -0,0 +1,29 @@
+//TODO: build out nav componentf
+// Base div
+  // Icon (emoji stand in)
+  // Links
+  // More button
+
+
+  export default function Nav(){
+    return (
+      // nav container
+      <div id="nav_container" className=""> 
+        {/* svg logo h1 rn */}
+        <h1 className="">
+          👻
+        </h1>
+        {/* Links div */}
+        <div className="">
+          {/* Links: Projects / About / Contact */}
+          <a>Projects</a>
+          <a>About</a>
+          <a>Contact</a>
+        </div>
+        {/* More button */}
+        <div className="">
+
+        </div>
+      </div>
+    )
+  }
---
+++ components/splash.js
@@ -0,0 +1,32 @@
+//TODO: Stub out component
+//TODO: Build out component
+//TODO: Style Component
+//TODO: Responsive / mobile design
+
+//Splash / Landing 
+// Logo Image 
+// Name Big
+// large-ish tagline 
+
+export default function Splash(){
+  return (
+    // Splash container
+    <div id="Splash" className="">
+      {/* Name Splash Div */}
+      <div className="">
+        {/* img logo eventually */}
+        <img className=""/>
+        {/* first name */}
+        <h1 className=""> Sean </h1>
+        {/* Last Name */}
+        <h1 className=""> Michael Feiner </h1>
+      </div>
+      {/* tagline div */}
+      <div className="">
+        {/* tagline text */}
+        <h3>I am a full stack developer, writer, and artist. I love information architecture, web development ecosystems, and open source development. </h3>
+        {/* other stuff ? */}
+      </div>
+    </div>
+  )
+}
