189ce170c63974e3b9ed7f6d709ef70119c5d499
sameghosts <seanfeiner.brown@gmail.com>
2021-04-05 17:27:59 -0400

    project card button responsive

--- components/projects.js
+++ components/projects.js
@@ -26,11 +26,11 @@ export default function Projects(){
           {/* project description */}
           <p className="p-2 font-semibold text-blue-600"> Restaurant match game for friends built using the MERN Stack and Yelp API. Collaborative team design and implementation. I was the back-end lead who designed the information architecture and ensured our database and server met the needs and logic for the client. </p>
           {/* project links */}
-          <div className="flex justify-between">
-          <a href="https://github.com/sameghosts/ChiknTindr-Server" target="_blank" className="rounded-full m-auto mt-2 mb-3 py-2 px-6 bg-red-600 hover:bg-red-100 text-red-200 hover:text-red-600 font-extrabold shadow-xl transition ease-in duration-150">
+          <div className="flex justify-between py-4">
+          <a href="https://github.com/sameghosts/ChiknTindr-Server" target="_blank" className="rounded-full m-auto mt-2 mb-3 py-2 px-6 bg-red-600 hover:bg-red-100 text-red-300 hover:text-red-600 sm:text-2xl font-extrabold shadow-xl transition ease-in duration-150">
             GitHub
           </a>
-          <a href="https://modest-visvesvaraya-3148b8.netlify.app/" target="_blank" className="rounded-full m-auto mt-2 mb-3 py-2 px-6 bg-red-600 hover:bg-red-100 text-red-200 hover:text-red-600 font-extrabold shadow-xl transition ease-in duration-150">
+          <a href="https://modest-visvesvaraya-3148b8.netlify.app/" target="_blank" className="rounded-full m-auto mt-2 mb-3 py-2 px-6 bg-red-600 hover:bg-red-100 text-red-300 hover:text-red-600 sm:text-2xl font-extrabold shadow-xl transition ease-in duration-150">
             Live Demo
           </a>
           </div>
@@ -38,7 +38,7 @@ export default function Projects(){
           {/* Img Icons */}
           <div className="flex flex-wrap justify-around items-center p-4">
             <img src="/logos/mongodb-ar21.svg" className="inline-block w-36 mr-4 " />
-            <img src="/logos/express.svg" className="inline-block p-2 w-1/6 mr-4 " />
+            <img src="/logos/express.svg" className="inline-block p-2 w-16 mr-4 " />
             <img src="/logos/react.svg" className="inline-block w-16 mr-4 " />
             <img src="/logos/nodejs-icon.svg" className="inline-block w-16 mr-4" />
           </div>
